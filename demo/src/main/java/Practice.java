263fd569478ad2c0385ee01bedfc11eb692784f5
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-03 12:01:40 -0500

    java loops 2 exercise

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -62,5 +62,22 @@ class Solution{
 //        for(int i = 1; i <= 10; i++) {
 //            System.out.printf("%d x %d = %d%n", N, i, N*i);
 //        }
+
+
+//        Scanner in = new Scanner(System.in);
+//        int t=in.nextInt();
+//        for(int i=0;i<t;i++){
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            int n = in.nextInt();
+//            int c = a;
+//            for(int j=0;j<n;j++){
+//                c += Math.pow(2, j)*b;
+//                System.out.printf("%s ",c);
+//            }
+//            System.out.println();
+//        }
+//        in.close();
+
     }
 }
