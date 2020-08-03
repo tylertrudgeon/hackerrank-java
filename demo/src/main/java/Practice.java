a5e8851fdbd25f11bf4fdc841459cde723697ba8
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-03 10:41:25 -0500

    java output formatting

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -46,6 +46,14 @@ class Solution{
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
-    }
 
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("================================");
+//        for(int i=0;i<3;i++){
+//            String s1=sc.next();
+//            int x=sc.nextInt();
+//            System.out.printf("%-15s%03d%n", s1, x);
+//        }
+//        System.out.println("================================");
+    }
 }
