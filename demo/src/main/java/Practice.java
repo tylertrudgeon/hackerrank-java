b8cf5a397651906c86718b95242225af9e5e7d8a
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-04 10:40:56 -0500

    java int to string

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -89,5 +89,24 @@ class Solution{
 //            i++;
 //        }
 
+
+//        try {
+//            Scanner in = new Scanner(System.in);
+//            int n = in .nextInt();
+//            in.close();
+//            //String s=???; Complete this line below
+//
+//            String s = Integer.toString(n);
+//
+//
+//            if (n == Integer.parseInt(s)) {
+//                System.out.println("Good job");
+//            } else {
+//                System.out.println("Wrong answer.");
+//            }
+//        } catch (DoNotTerminate.ExitTrappedException e) {
+//            System.out.println("Unsuccessful Termination!!");
+//        }
+
     }
 }
