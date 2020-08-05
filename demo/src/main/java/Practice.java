540726780c45e47a72bb3548c62113b05a405b19
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-05 12:51:09 -0500

    a very big sum answer

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -194,5 +194,10 @@ class Solution{
 //        return [alice, bob];
 //    }
 
+//        // Complete the aVeryBigSum function below.
+//        function aVeryBigSum(ar) {
+//        return ar.reduce((acc, curr) => acc + curr);
+//}
+
     }
 }
