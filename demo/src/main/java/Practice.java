6eb12d60fba18eaefd602f613cdc77effda16bb1
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-05 11:56:57 -0500

    compare the triplets problem done in JavaScript

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -179,5 +179,20 @@ class Solution{
 //        return total;
 //    }
 
+//        // Complete the compareTriplets function below.
+//        function compareTriplets(a, b) {
+//        let alice = 0;
+//        let bob = 0;
+//
+//        for(let i = 0; i < a.length; i ++) {
+//            if(a[i] > b[i]) {
+//                alice ++;
+//            } else  if (a[i] < b[i]) {
+//                bob ++;
+//            }
+//        }
+//        return [alice, bob];
+//    }
+
     }
 }
