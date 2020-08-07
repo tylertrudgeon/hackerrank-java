d9e0689c9e272e1927e1b9e546f27d3f6d5b9395
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-07 10:11:05 -0500

    staircase function

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -234,6 +234,13 @@ class Solution{
 //
 //}
 
+//        // Complete the staircase function below.
+//        function staircase(n) {
+//        for(let i = 1; i <= n; i++) {
+//            console.log( "#".repeat(i).padStart(n) );
+//        }
+//}
+
 
     }
 }
