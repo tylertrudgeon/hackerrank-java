9e103ae574b28f224fe557c96ca6927b61eba9ae
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-07 09:58:40 -0500

    plusMinus algorithm problem

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -210,6 +210,30 @@ class Solution{
 //        return (Math.abs(leftSum - rightSum));
 //}
 
+//        // Complete the plusMinus function below.
+//        function plusMinus(arr) {
+//            let pos = 0;
+//        let neg = 0;
+//        let zero = 0;
+//        let length = arr.length;
+//
+//        arr.forEach(n => {
+//        if(n > 0)
+//            pos++;
+//        else if(n < 0)
+//            neg++;
+//        else
+//            zero++;
+//    });
+//
+//        console.log( (pos / length).toFixed(6) );
+//        console.log( (neg / length).toFixed(6) );
+//        console.log( (zero / length).toFixed(6) );
+//
+//
+//
+//}
+
 
     }
 }
