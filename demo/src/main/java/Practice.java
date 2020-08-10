b2c66efe41dab54ff2df72921406e4ec8988a92d
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-10 11:04:30 -0500

    working on pizza project for OLO

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 
@@ -344,6 +346,53 @@ class Solution{
 //        function reverseArray(a) {
 //        return a.reverse();
 //}
+
+
+
+
+//        PIZZA PROJECT FOR OLO
+
+//        import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.Reader;
+//import java.net.URL;
+//import java.nio.charset.Charset;
+//import java.nio.charset.StandardCharsets;
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//
+//        public class PizzaProject {
+//            private static String readAll(Reader rd) throws IOException {
+//                StringBuilder sb = new StringBuilder();
+//                int cp;
+//                while ((cp = rd.read()) != -1) {
+//                    sb.append((char) cp);
+//                }
+//                return sb.toString();
+//            }
+//
+//            public static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
+//                InputStream is = new URL(url).openStream();
+//                try {
+//                    BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+//                    String jsonText = readAll(rd);
+//                    JSONArray json = new JSONArray(jsonText);
+//                    return json;
+//                } finally {
+//                    is.close();
+//                }
+//            }
+//
+//            public static void main(String[] args) throws IOException, JSONException {
+//                JSONArray json = readJsonFromUrl("https://www.olo.com/pizzas.json");
+//                System.out.println(json.toString());
+////        System.out.println(json.get("id"));
+//            }
+//        }
+
 
     }
 }
