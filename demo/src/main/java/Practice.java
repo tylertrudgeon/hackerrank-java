1cbb483bb060ed05162a5a6edb71d45e1b58df48
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-20 13:33:31 -0500

    birthday chocolate algorithm

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -402,6 +402,26 @@ class Solution{
 //    }
 
 
+//        // Complete the birthday function below.
+//        static int birthday(List<Integer> s, int d, int m) {
+//        int length = s.size();
+//        int numWays = 0, sum = 0;
+//
+//        for(int i = 0 ; i <= (length - m) ; i++){
+//            for(int j = 0 ; j < m ; j++){
+//                sum = sum + s.get(j+i);
+//            }
+//            if(sum == d){
+//                numWays++;
+//            }
+//            sum = 0;
+//        }
+//        return numWays;
+//
+//    }
+
+
+
 
 
 //        PIZZA PROJECT FOR OLO
