bdbd3ec897e03ea0cd8d7f1f68254f859be8a273
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-20 13:45:47 -0500

    divisible sums algorithm

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -420,6 +420,15 @@ class Solution{
 //
 //    }
 
+//        // Complete the divisibleSumPairs function below.
+//        static int divisibleSumPairs(int n, int k, int[] ar) {
+//        int count=0;
+//        for(int i=0; i < n; i++){
+//            for(int j=i+1; j<n; j++){
+//                count += ((ar[i] + ar[j]) % k == 0) ? 1 : 0;
+//            }
+//        }
+
 
 
 
