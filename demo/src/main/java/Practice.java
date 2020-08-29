67032ae15adfd1d76ad22e87b15f3eb9ecbc621f
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-08-28 23:30:20 -0500

    array manipulation data structure

--- demo/src/main/java/Practice.java
+++ demo/src/main/java/Practice.java
@@ -654,6 +654,32 @@ import java.util.List;
 //        return (i % 4 == 0 && i % 100 != 0)|| i%400 == 0 ? "12.09."+i : "13.09."+i;
 //        }
 //        }
+//
+//// Complete the arrayManipulation function below.
+//static long arrayManipulation(int n, int[][] queries) {
+//        long outputArray[] = new long[n + 2];
+//        for (int i = 0; i < queries.length; i++) {
+//        int a = queries[i][0];
+//        int b = queries[i][1];
+//        int k = queries[i][2];
+//        outputArray[a] += k;
+//        outputArray[b+1] -= k;
+//        }
+//        long max = getMax(outputArray);
+//        return max;
+//        }
+//
+//private static long getMax(long[] inputArray) {
+//        long max = Long.MIN_VALUE;
+//        long sum = 0;
+//        for (int i = 0; i < inputArray.length; i++) {
+//        sum += inputArray[i];
+//        max = Math.max(max, sum);
+//        }
+//        return max;
+//
+//        }
+
 
 
 
